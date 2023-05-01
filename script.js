3b99659fe2ecbaa49b2b5cd0e98d809c5e80622d
entering-a-username <tamunacer@gmail.com>
2023-05-01 18:32:18 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+(function() {
+    const card = document.getElementById("cardID");
+    const addAccount = document.getElementById("addAccountID");
+    const cancelAdding = document.getElementById("cancelAddingID");
+
+
+    addAccount.addEventListener("click", function() {
+        card.classList.remove("hidden");
+    })
+
+    cancelAdding.addEventListener("click", function() {
+        card.classList.add("hidden");
+    })
+
+    
+})();
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html", "script.js"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'facebook-font': ['SFProDisplay-Regular', 'Helvetica', 'Arial', 'sans-serif'],
+      }
+    },
+  },
+  plugins: [],
+}
